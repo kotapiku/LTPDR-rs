43f9f3aca7ef840e054e82c75e3ef2e27e405fe2
kotapiku <ice08235523@gmail.com>
2022-10-12 21:53:43 +0200

    add simple instance

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,37 +1,116 @@
 extern crate LTPDR_rs;
-use LTPDR_rs::*;
 use std::cmp;
+use std::collections::HashSet;
+use std::hash::Hash;
+use LTPDR_rs::*;
 
 fn main() {
-  let result = lt_pdr(f, alpha);
-  println!("{}", result);
+    let init = HashSet::from([1]);
+    let all = HashSet::from_iter(1..6);
+    let f = forward_ps(&init, &delta1);
+    let alpha = PS {
+        all,
+        subset: HashSet::from_iter(1..5),
+    };
+    let result = lt_pdr(&f, alpha);
+    println!("{}", result);
 }
 
-#[derive(Copy, Clone)]
-struct Nat {
-    n: u32,
-    is_top: bool,
+fn delta1(s: &u32) -> HashSet<u32> {
+    match s {
+        1 => HashSet::from([2, 3]),
+        2 => HashSet::from([1, 3]),
+        4 => HashSet::from([5]),
+        _ => HashSet::new(),
+    }
 }
 
-impl CLat for Nat {
-    type Info = ();
-    fn leq(&self, rhs: &Self) -> (bool, Self::Info) {
-        if !self.is_top & !rhs.is_top {
-            (self.n.le(&rhs.n), ())
-        } else {
-            (!self.is_top | rhs.is_top, ())
+fn forward_ps<'a, T: Eq + Hash + Clone>(
+    init: &'a HashSet<T>,
+    delta: &'a dyn Fn(&T) -> HashSet<T>,
+) -> impl Fn(&PS<T>) -> PS<T> + 'a {
+    |ps| {
+        let mut subset: HashSet<T> = HashSet::new();
+        subset = subset.union(init).cloned().collect();
+        for s in ps.subset.iter() {
+            subset = subset.union(&delta(s)).cloned().collect();
+        }
+        PS {
+            all: ps.all.clone(),
+            subset,
         }
     }
+}
 
-    fn bot(&self) -> Self { Nat { n: 0, is_top: false } }
-    fn top(&self) -> Self { Nat { n: 0, is_top: true } }
+#[derive(Debug)]
+struct PS<T> {
+    all: HashSet<T>,
+    subset: HashSet<T>,
+}
+
+impl<T: Eq + Hash + Clone> CLat for PS<T> {
+    type Info = ();
+    fn leq(&self, rhs: &Self) -> (bool, Self::Info) {
+        (self.subset.is_subset(&rhs.subset), ())
+    }
+    fn bot(&self) -> Self {
+        PS {
+            all: self.all.clone(),
+            subset: HashSet::new(),
+        }
+    }
+    fn top(&self) -> Self {
+        PS {
+            all: self.all.clone(),
+            subset: self.all.clone(),
+        }
+    }
     fn meet(&self, rhs: &Self) -> Self {
-        if self.is_top {
-            *rhs
-        } else if rhs.is_top {
-            *self
-        } else {
-            Nat { n: cmp::min(self.n, rhs.n), is_top: false }
+        PS {
+            all: self.all.clone(),
+            subset: self.subset.intersection(&rhs.subset).cloned().collect(),
+        }
+    }
+}
+
+impl<T: Clone + Hash + Eq> Heuristics<PS<T>> for PS<T> {
+    fn f_candidate(&self, alpha: &PS<T>, _: &<PS<T> as CLat>::Info) -> PS<T> {
+        PS {
+            all: self.all.clone(),
+            subset: self.subset.difference(&alpha.subset).cloned().collect(),
+        }
+    }
+    fn f_decide(
+        &self,
+        ci: &PS<T>,
+        f: &dyn Fn(&PS<T>) -> PS<T>,
+        _solver: &<PS<T> as CLat>::Info,
+    ) -> PS<T> {
+        let mut subset: HashSet<T> = self.subset.clone();
+        subset.retain(|x: &T| {
+            let fx: PS<T> = f(&PS {
+                all: self.all.clone(),
+                subset: HashSet::from([x.clone()]),
+            });
+            !(fx.subset.is_disjoint(&ci.subset))
+        });
+        PS {
+            all: self.all.clone(),
+            subset,
+        }
+    }
+    fn f_conflict(
+        &self,
+        ci: &PS<T>,
+        f: &dyn Fn(&PS<T>) -> PS<T>,
+        _solver: &<PS<T> as CLat>::Info,
+    ) -> PS<T> {
+        let fxi1 = f(self);
+        let ci_fxi1: HashSet<T> = ci.subset.difference(&fxi1.subset).cloned().collect();
+        let subset: HashSet<T> = self.all.difference(&ci_fxi1).cloned().collect();
+        PS {
+            all: self.all.clone(),
+            subset,
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use crate::PDRAnswer::*;
 use std::fmt;
+use std::fmt::Debug;
 
 pub trait CLat {
     // type Info T
@@ -12,6 +13,7 @@ pub trait CLat {
 
 type KTSeq<T> = Vec<T>;
 type KleeneSeq<T> = Vec<T>;
+#[derive(Debug)]
 struct KTKl<T>(KTSeq<T>, KleeneSeq<T>);
 pub enum PDRAnswer<T> {
     Valid(KTSeq<T>),
@@ -33,13 +35,14 @@ pub trait Heuristics<T: CLat> {
     fn f_conflict(&self, ci: &T, f: &dyn Fn(&T) -> T, solver: &T::Info) -> T;
 }
 
-pub fn lt_pdr<T: CLat + Heuristics<T>>(f: &dyn Fn(&T) -> T, alpha: T) -> PDRAnswer<T> {
+pub fn lt_pdr<T: CLat + Heuristics<T> + Debug>(f: &dyn Fn(&T) -> T, alpha: T) -> PDRAnswer<T> {
     // ([X_1, ..., X_{n-1}], [C_{n-1}, ..., C_i])
     let mut current = KTKl(vec![f(&alpha.bot())], Vec::new());
     loop {
+        println!("{:?}", &current);
         let KTKl(xs, cs) = &current;
 
-        if check_valid(&xs) {
+        if check_valid(xs) {
             return Valid(current.0); // valid
         }
         if xs.len() == cs.len() {
@@ -50,6 +53,7 @@ pub fn lt_pdr<T: CLat + Heuristics<T>>(f: &dyn Fn(&T) -> T, alpha: T) -> PDRAnsw
         let (result1, solver1) = xn1.leq(&alpha);
         if result1 {
             // unfold
+            println!("Unfold");
             current.0.push(alpha.top());
             current.1 = Vec::new();
             continue;
@@ -57,24 +61,28 @@ pub fn lt_pdr<T: CLat + Heuristics<T>>(f: &dyn Fn(&T) -> T, alpha: T) -> PDRAnsw
             match current.1.pop() {
                 None => {
                     // candidate
+                    println!("Candidate");
                     current.1.push(xn1.f_candidate(&alpha, &solver1));
                     continue;
-                },
+                }
                 Some(ci) => {
                     // ([X_1, ..., X_{n-1}], [C_{n-1}, ..., C_{i+1}])
+                    // n-1 - ((n-1) - (i+1) + 1) = i
                     let i = current.0.len() - current.1.len();
                     let xi1 = current.0.get(i - 2).unwrap();
                     let (result2, solver2) = ci.leq(&f(xi1));
                     if result2 {
                         // decide
+                        println!("Decide");
                         let x = xi1.f_decide(&ci, f, &solver2);
                         current.1.push(ci);
                         current.1.push(x);
                         continue;
                     } else {
                         // conflict
+                        println!("Conflict");
                         let x = xi1.f_conflict(&ci, f, &solver2);
-                        for v in current.1[..i].iter_mut() {
+                        for v in current.0[..i].iter_mut() {
                             *v = v.meet(&x);
                         }
                     }
@@ -87,8 +95,8 @@ pub fn lt_pdr<T: CLat + Heuristics<T>>(f: &dyn Fn(&T) -> T, alpha: T) -> PDRAnsw
 
 fn check_valid<T: CLat>(xs: &KTSeq<T>) -> bool {
     let mut result = false;
-    for i in 0..(xs.len()-1) {
-        result = result & (xs[i+1].leq(&xs[i]).0);
+    for i in 0..(xs.len() - 1) {
+        result |= xs[i + 1].leq(&xs[i]).0;
     }
     result
 }
